39f00fd308c9c91f142343d603b2a20c63910d2c
DS <>
2025-10-31 00:42:58 +0800

    Level-8 completed
    Added JavaDocs to moonchester_data

--- src/main/java/moonchester_data/Task.java
+++ src/main/java/moonchester_data/Task.java
@@ -1,11 +1,23 @@
 package moonchester_data;
 import moonchester_utils.MoonchesterException;
 
+/**
+ * Represents a general Task object
+ * Parent class for all types of tasks such as Todo, Deadline, and Event
+ * Contains the basic attributes and methods that are common across all task types
+ */
 public class Task {
     protected String description;
     protected boolean status;
 
-    public Task(String description) throws MoonchesterException{
+    /**
+     * Constructs a Task object with the specified description
+     * Throws a MoonchesterException exception if the description is empty or null
+     * 
+     * @param description Description of the task
+     * @throws MoonchesterException If the description is empty or null
+     */
+    public Task(String description) throws MoonchesterException {
         if (description.trim().isEmpty() == true || description == null) {
             throw new MoonchesterException("[!] Description cannot be empty.");
         }
@@ -13,25 +25,43 @@ public class Task {
         this.status = false;
     }
 
-    // Getters
-
+    /**
+     * Retrieves the description of the task
+     * 
+     * @return description Description of the task
+     */
     public String getDescription() {
         return this.description;
     }
 
+    /**
+     * Retrieves the status icon of the task
+     * Returns "X" if the task is completed, or a blank space otherwise
+     * 
+     * @return statusIcon "X" if done, " " if not done
+     */
     public String getStatusIcon() {
-        if(this.status == true) {
+        if (this.status == true) {
             return "X";
         }
         return " ";
     }
 
-    // Setters
-    public void setStatus(boolean status){
+    /**
+     * Updates the completion status of the task
+     * 
+     * @param status Boolean value indicating if the task is completed
+     */
+    public void setStatus(boolean status) {
         this.status = status;
     }
 
-    // For other subclasses to override
+    /**
+     * Returns the task details in a formatted string
+     * This method is meant to be overridden by subclasses for more specific formats
+     * 
+     * @return String Formatted string containing the task details
+     */
     public String printString() {
         return "[" + getStatusIcon() + "] " + getDescription();
     }
--- src/main/java/moonchester_data/UserList.java
+++ src/main/java/moonchester_data/UserList.java
@@ -2,21 +2,38 @@ package moonchester_data;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+
 import moonchester_utils.MoonchesterDate;
 
+/**
+ * Represents the list of tasks managed by the user
+ * Handles the storage, addition, deletion, and retrieval of Task objects
+ */
 public class UserList {
 
     private ArrayList<Task> userList = new ArrayList<>();
-
+    /**
+     * Constructs an empty UserList object
+     * Intialises a new ArrayList to store Task objects
+     */
     public UserList() {
         userList = new ArrayList<>();
     }
 
+    /**
+     * Constructs a UserList object with an existing list of tasks.
+     * 
+     * @param userList_init The initial ArrayList of Task objects to populate the UserList
+     */
     public UserList(ArrayList<Task> userList_init) {
         this.userList = userList_init;
     }
 
-    // Add Task to the userlist
+    /**
+     * Adds a task to the userList object
+     * 
+     * @param item Task to be added
+     */
     public void addItem (Task item) {
         userList.add(item);
         System.out.println("[+] Added : " + item.printString());
@@ -24,7 +41,12 @@ public class UserList {
         System.out.println("____________________________________________________________");
     }
 
-    // Delete Task to the userlist
+    /**
+     * Deletes a task from the userList object
+     * 
+     * @param item_index Index of the Task to be deleted
+     * @param item Task to be deleted
+     */
     public void deleteItem (int item_index, Task item) {
         userList.remove(item_index);
         System.out.println("[+] Deleted : " + item.printString());
@@ -32,12 +54,23 @@ public class UserList {
         System.out.println("____________________________________________________________");
     }
 
+    /**
+     * Returns the userList object as a new ArrayList<> object
+     * 
+     * @return ArrayList<Task> Current userList object
+     */
     public ArrayList<Task> getList () {
-        return userList;
+        return new ArrayList<>(userList);
     }
 
 
-    // Method overloading - Getlist based on the specific date given by user
+    /**
+     * Getlist based on the specific date given by user
+     * This is method overloading, the difference between the 2 methods is that this method takes in a specific date (LocalDateTime object)
+     * 
+     * @param specificDate Specific Date that user specifies, in LocalDateTime object
+     * @return queriedList ArrayList<Task> of Tasks that are due on a specific date
+     */
     public ArrayList<Task> getList(LocalDateTime specificDate) {
         ArrayList<Task> queriedList = new ArrayList<>();
         for(Task curr : userList) {
@@ -56,11 +89,20 @@ public class UserList {
     }
 
     // Get the specific task
+    /**
+     * Gets the specific task within the userList object
+     * 
+     * @param index Index of the specific task
+     * @return Task The specific Task
+     */
     public Task getSpecificTask(int index) {
         return this.userList.get(index-1);
     }
 
-    // Get the size of the userlist
+    /**
+     * Get the size of the userlist
+     * @return size Size of the userList object
+     */
     public int getSize() {
         return this.userList.size();
     }
